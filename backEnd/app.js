b64bd5e7945f351a3fb2c735d4ac2aa7dfca7afe
AntisShah <adrianshahnazari@gmail.com>
2026-02-14 23:53:07 -0500

    Additions to Backend
    app.js

---
+++ backEnd/app.js
@@ -0,0 +1,10 @@
+
+
+const event = {
+  name: "Adrian & Hossein's Potluck",
+  date: "2026-02-15",
+  slots: [
+    { id: 1, category: "Main", isClaimed: false, guestName: "" },
+    { id: 2, category: "Drink", isClaimed: true, guestName: "Hossein" }
+  ]
+};
